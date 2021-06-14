523dffead1f6feff752938dda4016e78561fa7c6
nicocalezich <ncalezich@gmail.com>
2021-06-14 19:07:19 -0300

    last change

--- src/components/Datos.vue
+++ src/components/Datos.vue
@@ -2,24 +2,26 @@
 
   <section class="src-components-datos"> 
 
-    <table class="table" v-if="hayDatos()">
-      <thead>
-        <tr class="table-primary">
-          <th class="table-primary" scope="col">Nombre</th>
-          <th class="table-primary" scope="col">Edad</th>
-          <th class="table-primary" scope="col">Mail</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(object,index) in data" :key="index" class="table-darprimaryk">
-          <td class="table-primary">{{object.nombre}}</td>
-          <td class="table-primary">{{object.edad}}</td>
-          <td class="table-primary">{{object.email}}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="data.length">
+      <table class="table">
+        <thead>
+          <tr class="table-primary">
+            <th class="table-primary" scope="col">Nombre</th>
+            <th class="table-primary" scope="col">Edad</th>
+            <th class="table-primary" scope="col">Mail</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(object,index) in data" :key="index" class="table-darprimaryk">
+            <td class="table-primary">{{object.nombre}}</td>
+            <td class="table-primary">{{object.edad}}</td>
+            <td class="table-primary">{{object.email}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
-    <div class="alert alert-warning" role="alert" v-else>
+    <div v-else class="alert alert-warning" role="alert">
       No se encontraron datos
     </div>
     
@@ -61,11 +63,8 @@
         catch(error) {
           console.error(error)
         }
-      },
-
-      hayDatos(){
-        return this.data.length > 0
       }
+      
     },
 
 
